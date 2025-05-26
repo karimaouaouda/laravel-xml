997bd6e24d24a9a59b1988474170b4449aa477f4
Karim Aouaouda <karimaouaouda.officiel@gmail.com>
2025-05-26 11:44:07 +0100

    fix bugs

--- resources/views/filament/resources/exercise-resource/widgets/exercise-widget.blade.php
+++ resources/views/filament/resources/exercise-resource/widgets/exercise-widget.blade.php
@@ -3,21 +3,21 @@
         <h1 class="w-full text-center pb-4 text-xl underline font-semibold capitalize">
             {{$title?? 'the exercise announcement'}}
         </h1>
-       
+
         @if($key == 'solution')
             @if($solution->exercise->require_xml)
             <div class="p-2 bg-white border rounded-md">
-                {!! $solution->xml_content !!}
+                {{ $solution->xml_content }}
             </div>
             @endif
             @if($solution->exercise->require_xsd)
             <div class="p-2 bg-white border rounded-md">
-                {!! $solution->xsd_content !!}
+                {{ $solution->xsd_content }}
             </div>
             @endif
             @if($solution->exercise->require_xslt)
             <div class="p-2 bg-white border rounded-md">
-                {!! $solution->xslt_content !!}
+                {{ $solution->xslt_content }}
             </div>
             @endif
         @else
