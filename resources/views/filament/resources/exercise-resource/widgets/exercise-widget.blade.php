3d671716cdd7792543fb307ec3b3a7aa8bef066b
Karim Aouaouda <karimaouaouda.officiel@gmail.com>
2025-05-26 11:55:04 +0100

    fix bugs

--- resources/views/filament/resources/exercise-resource/widgets/exercise-widget.blade.php
+++ resources/views/filament/resources/exercise-resource/widgets/exercise-widget.blade.php
@@ -5,21 +5,9 @@
         </h1>
 
         @if($key == 'solution')
-            @if($solution->exercise->require_xml)
             <div class="p-2 bg-white border rounded-md">
                 {{ $solution->xml_content }}
             </div>
-            @endif
-            @if($solution->exercise->require_xsd)
-            <div class="p-2 bg-white border rounded-md">
-                {{ $solution->xsd_content }}
-            </div>
-            @endif
-            @if($solution->exercise->require_xslt)
-            <div class="p-2 bg-white border rounded-md">
-                {{ $solution->xslt_content }}
-            </div>
-            @endif
         @else
             {!! $exercise->content !!}
         @endif
